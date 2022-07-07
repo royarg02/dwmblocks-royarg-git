royarg02/dwmblocks-royarg-git

commit 035a45125cf414bba331e82d1b8d86f5b50cb15f

Use scripts from repo itself for blocks

.SRCINFO
@@ -1,6 +1,6 @@
 pkgbase = dwmblocks-royarg-git
 	pkgdesc = A modified version of the modular status bar for dwm written in C.
-	pkgver = 1.0.f46922a
+	pkgver = 1.0.8f56741
 	pkgrel = 1
 	url = https://github.com/RoyARG02/dwmblocks
 	install = dwmblocks-royarg-git.install
@@ -19,18 +19,6 @@ pkgbase = dwmblocks-royarg-git
 	provides = dwmblocks
 	conflicts = dwmblocks
 	source = git+https://github.com/RoyARG02/dwmblocks.git
-	source = https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-battery
-	source = https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-clock
-	source = https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-memory
-	source = https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-nettraf
-	source = https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-pa-sink
-	source = https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-pa-source
 	md5sums = SKIP
-	md5sums = 93dd35a28c01d5c3321f5e840e8c83bb
-	md5sums = 649278834432ad707cb9ce4df0a6b159
-	md5sums = ed858e38ec51a983f1b9a9e53a0b51c3
-	md5sums = 8aca79f031f40cbd4bcdabf6eacf0855
-	md5sums = 9e43871322c76383d66bfe0b60930e86
-	md5sums = 15d9d20528f6bdefc7ac83184079ec7e
 
 pkgname = dwmblocks-royarg-git

PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Anurag Roy <anuragr9847@gmail.com>
 _pkgname="dwmblocks"
 pkgname="$_pkgname-royarg-git"
-pkgver=1.0.f46922a
+pkgver=1.0.8f56741
 pkgrel=1
 pkgdesc="A modified version of the modular status bar for dwm written in C."
 arch=('x86_64')
@@ -22,20 +22,8 @@ conflicts=("$_pkgname")
 install="$pkgname.install"
 source=(
   "git+$url.git"
-  https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-battery
-  https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-clock
-  https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-memory
-  https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-nettraf
-  https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-pa-sink
-  https://raw.githubusercontent.com/RoyARG02/user_files/master/files/scripts/sb-pa-source
 )
-md5sums=('SKIP'
-         '93dd35a28c01d5c3321f5e840e8c83bb'
-         '649278834432ad707cb9ce4df0a6b159'
-         'ed858e38ec51a983f1b9a9e53a0b51c3'
-         '8aca79f031f40cbd4bcdabf6eacf0855'
-         '9e43871322c76383d66bfe0b60930e86'
-         '15d9d20528f6bdefc7ac83184079ec7e')
+md5sums=('SKIP')
 
 pkgver() {
   cd "$_pkgname"
@@ -52,5 +40,4 @@ package() {
   make PREFIX=/usr DESTDIR="$pkgdir" install
   install -Dm644 README.md "$pkgdir"/usr/share/doc/$pkgname/README.md
   install -Dm644 LICENSE "$pkgdir"/usr/share/licenses/$pkgname/LICENSE
-  install -Dm755 "$startdir"/sb-* "$pkgdir"/usr/bin/
 }

commit c22f884e4e483d5be882fae97b3925209135545e

Fix version resolution by adding number of commits
The previous versioning only compared versions through commit hashes,
which are alphanumeric, and can lead to wrong determination of the newer
version.

For instance, the commit 035a45125cf414bba331e82d1b8d86f5b50cb15f broke
the database build due to the newer version(8f56741) being "lower" than
the older version(f46922a)

.SRCINFO
@@ -1,6 +1,6 @@
 pkgbase = dwmblocks-royarg-git
 	pkgdesc = A modified version of the modular status bar for dwm written in C.
-	pkgver = 1.0.8f56741
+	pkgver = 1.0.r75.8f56741c
 	pkgrel = 1
 	url = https://github.com/RoyARG02/dwmblocks
 	install = dwmblocks-royarg-git.install

PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Anurag Roy <anuragr9847@gmail.com>
 _pkgname="dwmblocks"
 pkgname="$_pkgname-royarg-git"
-pkgver=1.0.8f56741
+pkgver=1.0.r75.8f56741c
 pkgrel=1
 pkgdesc="A modified version of the modular status bar for dwm written in C."
 arch=('x86_64')
@@ -27,7 +27,9 @@ md5sums=('SKIP')
 
 pkgver() {
   cd "$_pkgname"
-  git describe --always --long | sed 's/^/1\.0\./'
+  commits="$(git rev-list --count HEAD)"
+  gitref="$(git rev-parse --short=8 HEAD)"
+  printf "1.0.r%s.%s" "$commits" "$gitref"
 }
 
 build() {